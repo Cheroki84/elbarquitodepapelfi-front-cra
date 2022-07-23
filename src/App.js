04b1d20b57f1063c5f2ac960b10833e6abb5c8b4
Cheroki84 <miguelangelmorato84@gmail.com>
2022-07-23 12:25:16 +0200

    fixed styles page under construction

--- src/App.js
+++ src/App.js
@@ -4,10 +4,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <div className='Text'>
+        <h1>
+          Sitio web en construcción
+        </h1>
+      </div>
+      <div className="centerFooter">
+        <div>
+          <p>
+            © 2020-{new Date().getFullYear()} El Barquito de Papel - Fotografía Infantil
+          </p>
+        </div>
+        <div>
+
+          <p>
+            Created by <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/miguelangelmorato84/">Miguel Ángel Morato</a>
+          </p>
+        </div>
+      </div>
+
     </div>
+
   );
 }
 
